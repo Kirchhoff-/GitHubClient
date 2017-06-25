2e8a1511a5cbbad80e2089e3f4f1fa20f1957c71
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-06-25 23:29:45 +0300

    Add application class.

---
+++ app/src/main/java/com/kirchhoff/example/githubclient/GitHubApplication.java
@@ -0,0 +1,15 @@
+package com.kirchhoff.example.githubclient;
+
+import android.app.Application;
+
+/**
+ * @author Kirchhoff-
+ */
+
+public class GitHubApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
