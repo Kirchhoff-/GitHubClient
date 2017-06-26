43f70eae46fa4d7f342973a6cd9f89eb2572b75b
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-06-26 20:48:28 +0300

    AuthActivity layout.

--- app/src/main/java/com/kirchhoff/example/githubclient/ui/auth/AuthActivity.java
+++ app/src/main/java/com/kirchhoff/example/githubclient/ui/auth/AuthActivity.java
@@ -1,6 +1,10 @@
 package com.kirchhoff.example.githubclient.ui.auth;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+
+import com.kirchhoff.example.githubclient.R;
 
 /**
  * @author Kirchhoff-
@@ -8,4 +12,9 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AuthActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_auth);
+    }
 }
