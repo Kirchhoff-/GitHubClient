35eb2ec22362738f634014349a792606c26c0cb9
Kirchhoff- <kirchhoff.19.92@gmail.com>
2017-07-04 20:42:25 +0300

    Added model classes.

---
+++ app/src/main/java/com/kirchhoff/example/githubclient/model/ModelClasses.kt
@@ -0,0 +1,24 @@
+package com.kirchhoff.example.githubclient.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @author Kirchhoff-
+ */
+data class Author(@SerializedName("name") val authorName: String)
+
+data class Authorization(@SerializedName("id") val id: Int,
+                         @SerializedName("token") val token: String)
+
+data class Commit(val repoName: String,
+                  @SerializedName("author") val author: Author,
+                  @SerializedName("message") val message: String)
+
+data class CommitResponse(@SerializedName("commit") val commit: Commit)
+
+data class Repository(@SerializedName("name") val name: String,
+                      @SerializedName("description") val description: String,
+                      @SerializedName("language") val language: String,
+                      @SerializedName("stargazers_count") val startCount: Int,
+                      @SerializedName("forks_count") val forksCount: Int,
+                      @SerializedName("watchers_count") val watchersCount: Int)
